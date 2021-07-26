9c36a3f36ca192f8aecaebd100e8e45f092570b1
Avishek <avishekprasadbarnwal@gamil.com>
2021-07-26 13:35:09 +0530

    add-redux-filter

--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,10 +1,28 @@
 // reducers declared what the action is 
-import { FETCH_PRODUCTS } from "../types";
+import { FETCH_PRODUCTS, FILTER_PRODUCT_BY_SIZE, ORDER_PRODUCT_BY_PRICE } from "../types";
 
 export const productsReducer = (state = {}, action) => {
     switch(action.type){
+        case FILTER_PRODUCT_BY_SIZE:
+            return {
+                ...state,
+                size: action.payload.size,
+                filteredItems: action.payload.items,
+            };
+
+        case ORDER_PRODUCT_BY_PRICE:
+            return {
+                ...state,
+                sort: action.payload.sort,
+                filteredItems: action.payload.items,
+            };
+
         case FETCH_PRODUCTS: 
-            return { items: action.payload };
+            return { 
+                items: action.payload ,
+                filteredItems: action.payload
+            };
+
         default :
             return state; 
     }
